8f2cf2ee02f99e74f0220e258f1c47a03e10c77e
姚晗 <741149770@qq.com>
2022-05-13 17:57:00 +0800

    功能实现

--- chineseTextOutputTool.py
+++ chineseTextOutputTool.py
@@ -26,12 +26,13 @@ def get_chinese_text(target_chinese_type):
         print("输入的中文类型错误, 只能是 Simplified 或者 Traditional !")
     
     # 拼接 检索目标 字符串
-    lang_str = 'lang \"' + target_chinese_type + '\"'
+    lang_str = lang_key_word + target_chinese_type + '\"'
     print("开始查找目标字符串: {}".format(lang_str))
 
     # 读取文件
     # 检测编码
     encoding_str = get_encoding(source_path)
+    # encoding_str = 'utf-8'
     with open(source_path, 'r', encoding = encoding_str) as f:
         data_strs = f.read().splitlines()
 
@@ -45,36 +46,31 @@ def get_chinese_text(target_chinese_type):
     
     # 循环, 找出不是中文的描述, 记录行号
     delete_index_list = []
+    lang_index_list = []
     for i, data_str in enumerate(data_strs):
         # 发现 lang
         if data_str.count(lang_key_word) > 0:
             # 如果不是本次需要的中文, 记录行号
             if data_str.find(target_chinese_type) == -1:
-                delete_index_list.append(i)
-                
-        # 如果行号 - 1 在列表中
-        if (i - 1) in delete_index_list:
-            # 如果是数字, 说明是 对应的描述行数 数字
-            if data_str.lstrip().isdecimal():
-                des_line_num = int(data_str.lstrip())
-            else:
-                # 如果不是, 则继续循环
-                continue
+                lang_index_list.append(i)
+    # 循环 待删索引列表, 目前是每种语言的头一行
+    for i in lang_index_list:
+        # 下一行, 一般是数字, 对应的描述行数 数字
+        data_str = data_strs[i + 1].lstrip()
+        if data_str.lstrip().isdecimal():
+            des_line_num = int(data_str.lstrip())
             # print("描述为: {}行".format(des_line_num))
-            delete_index_list.append(i)
-            for j in range(des_line_num, 0):
-                delete_index_list.append(j + des_line_num - 1)
-                # 删除 对应行数的文本
-                del data_strs[j + des_line_num - 1]
-            # 删除 对应的 lang
-            del data_strs[i - 1]
-            # 删除 本行 数字
-            del data_strs[i]
-    
+            delete_index_list.append(i + 1)
+            for j in range(des_line_num + 2):
+                delete_index_list.append(j + i + 1)
+    # 将lang 行号添加至 总列表中, 去重
+    delete_index_list = list(set(delete_index_list + lang_index_list))
+    # print("删除的索引列表 {}".format(delete_index_list))
     # 输出结果
     with open(target_path, 'w', encoding=encoding_str) as f:
-        for data_str in data_strs:
-            f.write(data_str + "\n")
+        for i, data_str in enumerate(data_strs):
+            if i not in delete_index_list:
+                f.write(data_str + "\n")
 
 
 if __name__ == "__main__":
